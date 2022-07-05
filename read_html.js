881b2abe757052324e71c88c4d97b7c079a71fdf
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2022-07-05 22:15:24 +0200

    test

--- read_html.js
+++ read_html.js
@@ -1,41 +1,60 @@
-const querystring = require('querystring');
-const mysql = require('mysql2');
+var querystring = require('querystring');
+var mysql = require('mysql2');
 
 //Connection mysql
-const con = mysql.createConnection({
+var con = mysql.createConnection({
     host: "localhost",
     user: "Nodejs",
     password: "NodeJS_MySQL",
 });
-
-function create_table(data) {
-    var table = "<table>";
-    table += "<tr><th>id</th><th>name</th><th>email</th></tr>";
-    for (var i = 0; i < data.length; i++) {
-      table += "<tr>";
-      table += "<td>" + data[i].id + "</td>";
-      table += "<td>" + data[i].name + "</td>";
-      table += "<td>" + data[i].email + "</td>";
-      table += "</tr>";
-    }
-    table += "</table>";
-    return table;
-  }
-  
-  exports.read_content = function (){
-  function cb (callback) {
-      console.log("callback called");
-      // on effectue une requête asynchrone
-      con.query("SELECT * FROM customers.customers", function(err, data) {
-        if(err) {
-          // s'il y a eu une erreur, on la renvoie à notre callback
-          callback(err);
-        } else {
-          // sinon, tout s'est bien passé, on traite les données puis on renvoie notre réponse au callback
-          console.log(data);
-          var table = create_table(data);
-          callback(null, table);
-        }
-      });
-    };
-};
+//fonction mysql
+con.query('SELECT * FROM customers.customers', function (err, rows) {
+    if (err) throw err;
+    querystring.stringify(rows);
+        exports.read_content = function compose_html () {return'<!Doctype html>' +
+                                '<html lang="fr">' +
+                                    '<head>' +
+                                        '<title>Read</title>' +
+                                        '<meta charset="utf-8">' +
+                                        '<style>'+
+                                        'html{' +
+                                        'font-family: Arial, Helvetica, sans-serif;' +
+                                        '}' +
+                                        'a{' +
+                                        'color:cornflowerblue' +
+                                        '}' +
+                                        'table {' +
+                                        'border-collapse: collapse;' +
+                                        'width: 100%;' +
+                                        '}' +
+                                        'th, td {' +
+                                        'padding: 8px;' +
+                                        'text-align: left;' +
+                                        'border-bottom: 1px solid #ddd;' +
+                                        '}' +
+                                        '</style>' +
+                                    '</head>' +
+                                    '<header>' +
+                                        '<h1>Read</h1>' +
+                                        '<a href="/">Accueil</a>' +
+                                        '<br>'+ 
+                                    '</header>' +
+                                    '<body>' +
+                                        '<table>' +
+                                        '<tr>' +
+                                        '<th>id</th>' +
+                                        '<th>name</th>' +
+                                        '<th>email</th>' +
+                                        '</tr>' +
+                                        rows.map(function(rows) {
+                                            return '<tr>' +
+                                            '<td>' + rows.id + '</td>' +
+                                            '<td>' + rows.name + '</td>' +
+                                            '<td>' + rows.email + '</td>' +
+                                            '</tr>'; 
+                                        }).join('')+
+                                        '</table>' +
+                                    '</body>' +
+                                '</html>';
+                            }
+});
